0ba245499176ba865e7fbba417ae3f03f614c80e
Steb2002 <stefanodeb02@gmail.com>
2025-06-23 22:29:20 +0200

    Create Spring project

---
+++ Code/progettoids/src/main/java/it/unicam/cs/diciotto/progettoids/ProgettoidsApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.diciotto.progettoids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoidsApplication.class, args);
+	}
+
+}
---
+++ Code/progettoids/src/test/java/it/unicam/cs/diciotto/progettoids/ProgettoidsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.diciotto.progettoids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
